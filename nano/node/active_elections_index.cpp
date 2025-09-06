pwojcikdev/nano-node

Introduce `active_elections_index`

nano/node/active_elections_index.cpp
@@ -1,0 +1,193 @@
+#include <nano/node/active_elections_index.hpp>
+#include <nano/node/election.hpp>
+
+#include <ranges>
+
+void nano::active_elections_index::insert (std::shared_ptr<nano::election> const & election, nano::election_behavior behavior, nano::bucket_index bucket, nano::priority_timestamp priority)
+{
+	debug_assert (!entries.get<tag_ptr> ().contains (election));
+	debug_assert (!entries.get<tag_root> ().contains (election->qualified_root));
+
+	auto [it, inserted] = entries.emplace_back (entry{ election, election->qualified_root, behavior, bucket, priority });
+	debug_assert (inserted);
+
+	// Update cached size
+	size_by_behavior[{ behavior }]++;
+	size_by_bucket[{ behavior, bucket }]++;
+}
+
+bool nano::active_elections_index::erase (std::shared_ptr<nano::election> const & election)
+{
+	auto maybe_entry = info (election);
+	if (!maybe_entry)
+	{
+		return false; // Not found
+	}
+	auto entry = *maybe_entry;
+
+	auto & index = entries.get<tag_ptr> ();
+	auto erased = index.erase (election);
+	release_assert (erased == 1);
+
+	// Update cached size
+	size_by_behavior[{ entry.behavior }]--;
+	size_by_bucket[{ entry.behavior, entry.bucket }]--;
+
+	return true; // Erased
+}
+
+void nano::active_elections_index::update (std::shared_ptr<nano::election> const & election, nano::election_behavior behavior)
+{
+	auto & index = entries.get<tag_ptr> ();
+	auto existing = index.find (election);
+	if (existing != index.end ())
+	{
+		auto old_behavior = existing->behavior;
+		auto bucket = existing->bucket;
+
+		if (old_behavior != behavior)
+		{
+			// Update cached sizes
+			size_by_behavior[{ old_behavior }]--;
+			size_by_bucket[{ old_behavior, bucket }]--;
+			size_by_behavior[{ behavior }]++;
+			size_by_bucket[{ behavior, bucket }]++;
+
+			// Update the entry
+			index.modify (existing, [behavior] (entry & e) {
+				e.behavior = behavior;
+			});
+		}
+	}
+	else
+	{
+		debug_assert (false, "election not found in index");
+	}
+}
+
+bool nano::active_elections_index::exists (nano::qualified_root const & root) const
+{
+	return entries.get<tag_root> ().contains (root);
+}
+
+bool nano::active_elections_index::exists (std::shared_ptr<nano::election> const & election) const
+{
+	return entries.get<tag_ptr> ().contains (election);
+}
+
+std::shared_ptr<nano::election> nano::active_elections_index::election (nano::qualified_root const & root) const
+{
+	if (auto existing = entries.get<tag_root> ().find (root); existing != entries.get<tag_root> ().end ())
+	{
+		return existing->election;
+	}
+	return nullptr;
+}
+
+auto nano::active_elections_index::info (std::shared_ptr<nano::election> const & election) const -> std::optional<entry>
+{
+	if (auto existing = entries.get<tag_ptr> ().find (election); existing != entries.get<tag_ptr> ().end ())
+	{
+		return *existing;
+	}
+	return std::nullopt;
+}
+
+auto nano::active_elections_index::list () const -> std::deque<entry>
+{
+	auto r = entries.get<tag_sequenced> () | std::views::transform ([] (auto const & entry) { return entry; });
+	return { r.begin (), r.end () };
+}
+
+size_t nano::active_elections_index::size () const
+{
+	return entries.size ();
+}
+
+size_t nano::active_elections_index::size (nano::election_behavior behavior) const
+{
+	if (auto existing = size_by_behavior.find ({ behavior }); existing != size_by_behavior.end ())
+	{
+		return existing->second;
+	}
+	return 0;
+}
+
+size_t nano::active_elections_index::size (nano::election_behavior behavior, nano::bucket_index bucket) const
+{
+	if (auto existing = size_by_bucket.find ({ behavior, bucket }); existing != size_by_bucket.end ())
+	{
+		return existing->second;
+	}
+	return 0;
+}
+
+auto nano::active_elections_index::highest (nano::election_behavior behavior, nano::bucket_index bucket) const -> priority_result
+{
+	auto & index = entries.get<tag_key> ();
+
+	// Returns an iterator pointing to the first element with key greater than x
+	auto existing = index.upper_bound (key{ behavior, bucket, std::numeric_limits<nano::priority_timestamp>::max () });
+	if (existing != index.begin ())
+	{
+		--existing;
+		if (existing->behavior == behavior && existing->bucket == bucket)
+		{
+			return { existing->election, existing->priority };
+		}
+	}
+
+	return { nullptr, std::numeric_limits<nano::priority_timestamp>::max () };
+}
+
+auto nano::active_elections_index::list (std::chrono::steady_clock::time_point cutoff, std::chrono::steady_clock::time_point now) -> std::deque<std::shared_ptr<nano::election>>
+{
+	auto & index = entries.get<tag_timestamp> ();
+
+	// Collect entries to process first to avoid iterator invalidation issues
+	std::deque<decltype (index.begin ())> to_process;
+	auto end = index.upper_bound (cutoff);
+	for (auto it = index.begin (); it != end; ++it)
+	{
+		to_process.push_back (it);
+	}
+
+	// Process and update timestamps
+	for (auto it : to_process)
+	{
+		// Update timestamp to 'now' for processed entries
+		index.modify (it, [now] (entry & e) {
+			e.timestamp = now;
+		});
+	}
+
+	auto r = to_process | std::views::transform ([] (auto const & it) { return it->election; });
+	return { r.begin (), r.end () };
+}
+
+bool nano::active_elections_index::trigger (std::shared_ptr<nano::election> const & election)
+{
+	auto & index = entries.get<tag_ptr> ();
+	if (auto existing = index.find (election); existing != index.end ())
+	{
+		index.modify (existing, [] (entry & e) {
+			e.timestamp = {};
+		});
+		return true;
+	}
+	return false; // Not found
+}
+
+bool nano::active_elections_index::any (std::chrono::steady_clock::time_point cutoff) const
+{
+	auto & index = entries.get<tag_timestamp> ();
+	auto it = index.begin ();
+	return it != index.end () && it->timestamp <= cutoff;
+}
+
+void nano::active_elections_index::clear ()
+{
+	entries.clear ();
+	size_by_behavior.clear ();
+	size_by_bucket.clear ();
+}

nano/node/active_elections_index.hpp
@@ -1,0 +1,121 @@
+#pragma once
+
+#include <nano/lib/numbers.hpp>
+#include <nano/lib/numbers_templ.hpp>
+#include <nano/node/fwd.hpp>
+#include <nano/secure/common.hpp>
+
+#include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/mem_fun.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/random_access_index.hpp>
+#include <boost/multi_index/sequenced_index.hpp>
+#include <boost/multi_index_container.hpp>
+
+#include <chrono>
+#include <deque>
+#include <functional>
+#include <map>
+#include <optional>
+#include <set>
+#include <unordered_map>
+
+namespace mi = boost::multi_index;
+
+namespace nano
+{
+class active_elections_index
+{
+public:
+	struct key
+	{
+		nano::election_behavior behavior;
+		nano::bucket_index bucket;
+		nano::priority_timestamp priority;
+
+		auto operator<=> (key const &) const = default;
+	};
+
+	struct entry
+	{
+		std::shared_ptr<nano::election> election;
+
+		nano::qualified_root root;
+		nano::election_behavior behavior;
+		nano::bucket_index bucket;
+		nano::priority_timestamp priority;
+
+		std::chrono::steady_clock::time_point timestamp{};
+
+		active_elections_index::key key () const
+		{
+			return { behavior, bucket, priority };
+		}
+	};
+
+public:
+	void insert (std::shared_ptr<nano::election> const &, nano::election_behavior, nano::bucket_index, nano::priority_timestamp);
+	bool erase (std::shared_ptr<nano::election> const &);
+	void update (std::shared_ptr<nano::election> const &, nano::election_behavior);
+
+	bool exists (nano::qualified_root const &) const;
+	bool exists (std::shared_ptr<nano::election> const &) const;
+
+	std::shared_ptr<nano::election> election (nano::qualified_root const &) const;
+	std::optional<entry> info (std::shared_ptr<nano::election> const &) const;
+
+	size_t size () const;
+	size_t size (nano::election_behavior) const;
+	size_t size (nano::election_behavior, nano::bucket_index) const;
+
+	// Returns election with the highest priority value. NOTE: Lower "priority" is better
+	using priority_result = std::pair<std::shared_ptr<nano::election>, nano::priority_timestamp>;
+	priority_result highest (nano::election_behavior, nano::bucket_index) const;
+
+	std::deque<entry> list () const;
+
+	// Return list of elections with a timestamp before the specified cutoff time
+	std::deque<std::shared_ptr<nano::election>> list (
+	std::chrono::steady_clock::time_point cutoff,
+	std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now ());
+
+	// Mark an election for update (reset its timestamp)
+	bool trigger (std::shared_ptr<nano::election> const &);
+
+	// Are there any elections with a timestamp before the specified cutoff time
+	bool any (std::chrono::steady_clock::time_point cutoff) const;
+
+	void clear ();
+
+private:
+	// clang-format off
+	class tag_sequenced {};
+	class tag_root {};
+	class tag_ptr {};
+	class tag_key {};
+	class tag_timestamp {};
+
+	using ordered_entries = boost::multi_index_container<entry,
+	mi::indexed_by<
+		mi::sequenced<mi::tag<tag_sequenced>>,
+		mi::hashed_unique<mi::tag<tag_root>,
+			mi::member<entry, nano::qualified_root, &entry::root>>,
+		mi::hashed_unique<mi::tag<tag_ptr>,
+		    mi::member<entry, std::shared_ptr<nano::election>, &entry::election>>,
+		mi::ordered_non_unique<mi::tag<tag_key>,
+			mi::const_mem_fun<entry, key, &entry::key>>,
+		mi::ordered_non_unique<mi::tag<tag_timestamp>,
+			mi::member<entry, std::chrono::steady_clock::time_point, &entry::timestamp>>
+	>>;
+	// clang-format on
+	ordered_entries entries;
+
+	// Keep track of the total number of elections to provide constant time lookups
+	using behavior_key_t = nano::election_behavior;
+	std::map<behavior_key_t, size_t> size_by_behavior;
+
+	using bucket_key_t = std::pair<nano::election_behavior, nano::bucket_index>;
+	std::map<bucket_key_t, size_t> size_by_bucket;
+};
+}

empresascorea0-alt/Lumex-node

nano/node/active_elections_index.cpp
@@ -1,0 +1,191 @@
+#include <nano/node/active_elections_index.hpp>
+#include <nano/node/election.hpp>
+
+#include <ranges>
+
+void nano::active_elections_index::insert (std::shared_ptr<nano::election> const & election, nano::election_behavior behavior, nano::bucket_index bucket, nano::priority_timestamp priority)
+{
+	debug_assert (!entries.get<tag_ptr> ().contains (election));
+	debug_assert (!entries.get<tag_root> ().contains (election->qualified_root));
+
+	auto [it, inserted] = entries.emplace_back (entry{ election, election->qualified_root, behavior, bucket, priority });
+	debug_assert (inserted);
+
+	// Update cached size
+	size_by_behavior[{ behavior }]++;
+	size_by_bucket[{ behavior, bucket }]++;
+}
+
+bool nano::active_elections_index::erase (std::shared_ptr<nano::election> const & election)
+{
+	auto maybe_entry = info (election);
+	if (!maybe_entry)
+	{
+		return false; // Not found
+	}
+	auto entry = *maybe_entry;
+
+	auto & index = entries.get<tag_ptr> ();
+	auto erased = index.erase (election);
+	release_assert (erased == 1);
+
+	// Update cached size
+	size_by_behavior[{ entry.behavior }]--;
+	size_by_bucket[{ entry.behavior, entry.bucket }]--;
+
+	return true; // Erased
+}
+
+void nano::active_elections_index::update (std::shared_ptr<nano::election> const & election, nano::election_behavior behavior)
+{
+	auto & index = entries.get<tag_ptr> ();
+	auto existing = index.find (election);
+	if (existing != index.end ())
+	{
+		auto old_behavior = existing->behavior;
+		auto bucket = existing->bucket;
+
+		if (old_behavior != behavior)
+		{
+			// Update cached sizes
+			size_by_behavior[{ old_behavior }]--;
+			size_by_bucket[{ old_behavior, bucket }]--;
+			size_by_behavior[{ behavior }]++;
+			size_by_bucket[{ behavior, bucket }]++;
+
+			// Update the entry
+			index.modify (existing, [behavior] (entry & e) {
+				e.behavior = behavior;
+			});
+		}
+	}
+	else
+	{
+		debug_assert (false, "election not found in index");
+	}
+}
+
+bool nano::active_elections_index::exists (nano::qualified_root const & root) const
+{
+	return entries.get<tag_root> ().contains (root);
+}
+
+bool nano::active_elections_index::exists (std::shared_ptr<nano::election> const & election) const
+{
+	return entries.get<tag_ptr> ().contains (election);
+}
+
+std::shared_ptr<nano::election> nano::active_elections_index::election (nano::qualified_root const & root) const
+{
+	if (auto existing = entries.get<tag_root> ().find (root); existing != entries.get<tag_root> ().end ())
+	{
+		return existing->election;
+	}
+	return nullptr;
+}
+
+auto nano::active_elections_index::info (std::shared_ptr<nano::election> const & election) const -> std::optional<entry>
+{
+	if (auto existing = entries.get<tag_ptr> ().find (election); existing != entries.get<tag_ptr> ().end ())
+	{
+		return *existing;
+	}
+	return std::nullopt;
+}
+
+auto nano::active_elections_index::list () const -> std::deque<entry>
+{
+	auto r = entries.get<tag_sequenced> () | std::views::transform ([] (auto const & entry) { return entry; });
+	return { r.begin (), r.end () };
+}
+
+size_t nano::active_elections_index::size () const
+{
+	return entries.size ();
+}
+
+size_t nano::active_elections_index::size (nano::election_behavior behavior) const
+{
+	if (auto existing = size_by_behavior.find ({ behavior }); existing != size_by_behavior.end ())
+	{
+		return existing->second;
+	}
+	return 0;
+}
+
+size_t nano::active_elections_index::size (nano::election_behavior behavior, nano::bucket_index bucket) const
+{
+	if (auto existing = size_by_bucket.find ({ behavior, bucket }); existing != size_by_bucket.end ())
+	{
+		return existing->second;
+	}
+	return 0;
+}
+
+auto nano::active_elections_index::last (nano::election_behavior behavior, nano::bucket_index bucket) const -> priority_result
+{
+	auto & index = entries.get<tag_key> ();
+
+	// Find the range of entries with matching behavior and bucket
+	auto range = index.equal_range (std::make_tuple (behavior, bucket));
+	if (range.first != range.second)
+	{
+		// Since the index is ordered, the last element has the highest priority (largest value)
+		auto last = std::prev (range.second);
+		return { last->election, last->priority };
+	}
+
+	return { nullptr, std::numeric_limits<nano::priority_timestamp>::max () };
+}
+
+auto nano::active_elections_index::list (std::chrono::steady_clock::time_point cutoff, std::chrono::steady_clock::time_point now) -> std::deque<std::shared_ptr<nano::election>>
+{
+	auto & index = entries.get<tag_timestamp> ();
+
+	// Collect entries to process first to avoid iterator invalidation issues
+	std::deque<decltype (index.begin ())> to_process;
+	auto end = index.upper_bound (cutoff);
+	for (auto it = index.begin (); it != end; ++it)
+	{
+		to_process.push_back (it);
+	}
+
+	// Process and update timestamps
+	for (auto it : to_process)
+	{
+		// Update timestamp to 'now' for processed entries
+		index.modify (it, [now] (entry & e) {
+			e.timestamp = now;
+		});
+	}
+
+	auto r = to_process | std::views::transform ([] (auto const & it) { return it->election; });
+	return { r.begin (), r.end () };
+}
+
+bool nano::active_elections_index::trigger (std::shared_ptr<nano::election> const & election)
+{
+	auto & index = entries.get<tag_ptr> ();
+	if (auto existing = index.find (election); existing != index.end ())
+	{
+		index.modify (existing, [] (entry & e) {
+			e.timestamp = {};
+		});
+		return true;
+	}
+	return false; // Not found
+}
+
+bool nano::active_elections_index::any (std::chrono::steady_clock::time_point cutoff) const
+{
+	auto & index = entries.get<tag_timestamp> ();
+	auto it = index.begin ();
+	return it != index.end () && it->timestamp <= cutoff;
+}
+
+void nano::active_elections_index::clear ()
+{
+	entries.clear ();
+	size_by_behavior.clear ();
+	size_by_bucket.clear ();
+}

nano/node/active_elections_index.hpp
@@ -1,0 +1,111 @@
+#pragma once
+
+#include <nano/lib/numbers.hpp>
+#include <nano/lib/numbers_templ.hpp>
+#include <nano/node/fwd.hpp>
+#include <nano/secure/common.hpp>
+
+#include <boost/multi_index/composite_key.hpp>
+#include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/mem_fun.hpp>
+#include <boost/multi_index/member.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/random_access_index.hpp>
+#include <boost/multi_index/sequenced_index.hpp>
+#include <boost/multi_index_container.hpp>
+
+#include <chrono>
+#include <deque>
+#include <functional>
+#include <map>
+#include <optional>
+#include <set>
+#include <unordered_map>
+
+namespace mi = boost::multi_index;
+
+namespace nano
+{
+class active_elections_index
+{
+public:
+	struct entry
+	{
+		std::shared_ptr<nano::election> election;
+
+		nano::qualified_root root;
+		nano::election_behavior behavior;
+		nano::bucket_index bucket;
+		nano::priority_timestamp priority;
+
+		std::chrono::steady_clock::time_point timestamp{};
+	};
+
+public:
+	void insert (std::shared_ptr<nano::election> const &, nano::election_behavior, nano::bucket_index, nano::priority_timestamp);
+	bool erase (std::shared_ptr<nano::election> const &);
+	void update (std::shared_ptr<nano::election> const &, nano::election_behavior);
+
+	bool exists (nano::qualified_root const &) const;
+	bool exists (std::shared_ptr<nano::election> const &) const;
+
+	std::shared_ptr<nano::election> election (nano::qualified_root const &) const;
+	std::optional<entry> info (std::shared_ptr<nano::election> const &) const;
+
+	size_t size () const;
+	size_t size (nano::election_behavior) const;
+	size_t size (nano::election_behavior, nano::bucket_index) const;
+
+	// Returns election with the highest priority value. NOTE: Lower "priority" is better
+	using priority_result = std::pair<std::shared_ptr<nano::election>, nano::priority_timestamp>;
+	priority_result last (nano::election_behavior, nano::bucket_index) const;
+
+	std::deque<entry> list () const;
+
+	// Return list of elections with a timestamp before the specified cutoff time
+	std::deque<std::shared_ptr<nano::election>> list (
+	std::chrono::steady_clock::time_point cutoff,
+	std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now ());
+
+	// Mark an election for update (reset its timestamp)
+	bool trigger (std::shared_ptr<nano::election> const &);
+
+	// Are there any elections with a timestamp before the specified cutoff time
+	bool any (std::chrono::steady_clock::time_point cutoff) const;
+
+	void clear ();
+
+private:
+	// clang-format off
+	class tag_sequenced {};
+	class tag_root {};
+	class tag_ptr {};
+	class tag_key {};
+	class tag_timestamp {};
+
+	using ordered_entries = boost::multi_index_container<entry,
+	mi::indexed_by<
+		mi::sequenced<mi::tag<tag_sequenced>>,
+		mi::hashed_unique<mi::tag<tag_root>,
+			mi::member<entry, nano::qualified_root, &entry::root>>,
+		mi::hashed_unique<mi::tag<tag_ptr>,
+		    mi::member<entry, std::shared_ptr<nano::election>, &entry::election>>,
+		mi::ordered_non_unique<mi::tag<tag_key>,
+			mi::composite_key<entry,
+				mi::member<entry, nano::election_behavior, &entry::behavior>,
+				mi::member<entry, nano::bucket_index, &entry::bucket>,
+				mi::member<entry, nano::priority_timestamp, &entry::priority>>>,
+		mi::ordered_non_unique<mi::tag<tag_timestamp>,
+			mi::member<entry, std::chrono::steady_clock::time_point, &entry::timestamp>>
+	>>;
+	// clang-format on
+	ordered_entries entries;
+
+	// Keep track of the total number of elections to provide constant time lookups
+	using behavior_key_t = nano::election_behavior;
+	std::map<behavior_key_t, size_t> size_by_behavior;
+
+	using bucket_key_t = std::pair<nano::election_behavior, nano::bucket_index>;
+	std::map<bucket_key_t, size_t> size_by_bucket;
+};
+}