10d4780e01f78c51dd7f9827d838726e96c70bed
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2026-02-13 16:35:38 +0100

    Clean up `ledger_set` header declarations
    Qualify `secure::transaction` as `nano::secure::transaction`
    and remove unused parameter names from declarations.

--- nano/secure/ledger_set_cemented.hpp
+++ nano/secure/ledger_set_cemented.hpp
@@ -15,30 +15,30 @@ public:
 	explicit ledger_set_cemented (nano::ledger const &);
 
 public: // Operations on accounts
-	std::optional<nano::amount> account_balance (secure::transaction const & transaction, nano::account const & account) const;
-	nano::block_hash account_head (secure::transaction const & transaction, nano::account const & account) const;
-	uint64_t account_height (secure::transaction const & transaction, nano::account const & account) const;
+	std::optional<nano::amount> account_balance (nano::secure::transaction const &, nano::account const &) const;
+	nano::block_hash account_head (nano::secure::transaction const &, nano::account const &) const;
+	uint64_t account_height (nano::secure::transaction const &, nano::account const &) const;
 
 public: // Operations on blocks
-	std::optional<nano::amount> block_balance (secure::transaction const & transaction, nano::block_hash const & hash) const;
-	bool block_exists (secure::transaction const & transaction, nano::block_hash const & hash) const;
-	bool block_exists (secure::transaction const & transaction, nano::block const & block) const;
-	bool block_exists_or_pruned (secure::transaction const & transaction, nano::block_hash const & hash) const;
-	std::shared_ptr<nano::block> block_get (secure::transaction const & transaction, nano::block_hash const & hash) const;
+	std::optional<nano::amount> block_balance (nano::secure::transaction const &, nano::block_hash const &) const;
+	bool block_exists (nano::secure::transaction const &, nano::block_hash const &) const;
+	bool block_exists (nano::secure::transaction const &, nano::block const &) const;
+	bool block_exists_or_pruned (nano::secure::transaction const &, nano::block_hash const &) const;
+	std::shared_ptr<nano::block> block_get (nano::secure::transaction const &, nano::block_hash const &) const;
 
 public: // Operations on pending entries
 	receivable_iterator receivable_end () const;
 	// Returns the next receivable entry equal or greater than 'key'
 	// Mirrors std::map::lower_bound
-	std::optional<std::pair<nano::pending_key, nano::pending_info>> receivable_lower_bound (secure::transaction const & transaction, nano::account const & account, nano::block_hash const & hash) const;
+	std::optional<std::pair<nano::pending_key, nano::pending_info>> receivable_lower_bound (nano::secure::transaction const &, nano::account const &, nano::block_hash const &) const;
 	// Returns the next receivable entry for an account greater than 'account'
 	// Returns receivable_lower_bound (transaction, account + 1, 0)
 	// Mirrors std::map::upper_bound
-	receivable_iterator receivable_upper_bound (secure::transaction const & transaction, nano::account const & account) const;
+	receivable_iterator receivable_upper_bound (nano::secure::transaction const &, nano::account const &) const;
 	// Returns the next receivable entry for the account 'account' with hash greater than 'hash'
 	// Returns receivable_lower_bound (transaction, account + 1, hash)
 	// Mirrors std::map::upper_bound
-	receivable_iterator receivable_upper_bound (secure::transaction const & transaction, nano::account const & account, nano::block_hash const & hash) const;
+	receivable_iterator receivable_upper_bound (nano::secure::transaction const &, nano::account const &, nano::block_hash const &) const;
 
 private:
 	nano::ledger const & ledger;
